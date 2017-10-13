b13c5411b5271d0e6f3f40eb17b975e4b9a53424
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-10-13 10:42:14 +0200

    recherche pour ouvrir un fichier json

--- docs/js/chartPie.js
+++ docs/js/chartPie.js
@@ -40,19 +40,44 @@ function drawChart() {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Topping');
     data.addColumn('number', 'Slices');
-    data.addRows([
+    var dataJson = $.getJSON("repo.json", function(json){
+        return json;
+    });//ouverture et lecture fichier json
+
+
+
+
+    
+//TODO: VOIR DANS INDEX.HTML POUR INSERER JQUERY!!!! REGARDER DANS LA CONSOLE DE FIREFOX LES ERREURS A REGLER!!!
+
+
+
+
+
+
+
+
+
+    console.log(dataJson);
+    var result = [];
+    for(var i in dataJson){
+        result.push([i, dataJson[i]]);
+    }
+    data.addRows(result);
+
+    /*data.addRows([
         ['James', 31],
         ['Onions', 1],
         ['Olives', 1],
         ['Zucchini', 1],
         ['Pepperoni', 2]
-    ]);
+    ]);*/
 
     // Set chart options
     var options = {
-        'title': 'How Much Pizza I Ate Last Night',
-        'width': 400,
-        'height': 300
+        'title': 'Number of commit by author',
+        'width': 800,
+        'height': 700
     };
 
     // Instantiate and draw our chart, passing in some options.
