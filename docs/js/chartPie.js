6cb00ea9ee10654bd8ac7b6ccabdd38d2890f5f2
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-10-14 15:41:08 +0200

    modif du visuel

--- docs/js/chartPie.js
+++ docs/js/chartPie.js
@@ -8,13 +8,19 @@ function drawChart() {
     const data = google.visualization.arrayToDataTable(dataJson.data);
     function showFullTooltip(row, size, value) {
       return `${'<div style="background:#fd9; padding:10px; border-style:solid">' +
-                  '<span style="font-family:Courier"><b>'}${data.getValue(row, 0)
+        '<span style="font-family:Courier"><b> NOM: '}${data.getValue(row, 0)}</b></span><br>` +
+        `${data.getColumnLabel(4)}: ${data.getValue(row, 4)}<br>${
+        data.getColumnLabel(5)}: ${data.getValue(row, 5)}<br>${
+        data.getColumnLabel(6)}:<a style="color: #BE00FF;"  target="_blank" href=${data.getValue(row, 6)}>
+            ${data.getValue(row, 6)}</a> </div>`;
+      /* return `${'<div style="background:#fd9; padding:10px; border-style:solid">' +
+                  '<span style="font-family:Courier"><b> NOM: '}${data.getValue(row, 0)
       }</b>, ${data.getValue(row, 1)}, ${data.getValue(row, 2)
       }, ${data.getValue(row, 3)}</span><br>` +
                   `Datatable row: ${row}<br>${
                     data.getColumnLabel(2)
                   } (total value of this cell and its children): ${size}<br>${
-                    data.getColumnLabel(3)}: ${value} </div>`; // + '<br>' + data.getColumnLabel(4) + data.getValue(row, 4)
+                    data.getColumnLabel(3)}: ${value} </div>`; */ // + '<br>' + data.getColumnLabel(4) + data.getValue(row, 4)
     }
 
     const options = {
